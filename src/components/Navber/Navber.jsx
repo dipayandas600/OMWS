d95199ef6e91a1744fbf21aba458f8a76e0ea319
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-07-31 14:03:43 -0700

    update css and jsx files.

--- src/components/Navber/Navber.jsx
+++ src/components/Navber/Navber.jsx
@@ -3,25 +3,31 @@ import './Navber.css'
 
 const Navber = () => {
   return (
-    <div className="navber">
-        <div className="right-side">
-            <h1 className="logo">OMWS</h1>
-        </div>
-        
-        <div className="meddle">
-            <ul className="navber-links">
-                <li>Home</li>
-                <li>Series</li>
-                <li>Tv Shows</li>
-                <li>Movies</li>
-                <li>My Lists</li>
-                <li>Gondras</li>
-            </ul>
-        </div>
-        
-        <div className="left-side">
-            <input type="text" placeholder='search-here' className='search-box' /><button className="search-button"></button>
-            <button className="user-button"><img src="" alt="" className='user-icon' /></button>
+    <div className="header">
+        <div className="navber">
+            <div className="right-side">
+                <h1 className="logo">OMWS</h1>
+            </div>
+            
+            <div className="meddle">
+                <ul className="navber-links">
+                    <li>Home</li>
+                    <li>Series</li>
+                    <li>Tv Shows</li>
+                    <li>Movies</li>
+                    <li>My Lists</li>
+                    <li>Gondras</li>
+                </ul>
+            </div>
+            
+            <div className="left-side">
+                <div className="search-box">
+                    <input type="text" placeholder='search-here' /><button className="search-button"></button>
+                </div>
+                <div className="user">
+                    <button className="user-button"></button>
+                </div>
+            </div>
         </div>
     </div>
   )
