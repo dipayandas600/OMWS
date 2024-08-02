8e78398b6c00225d27b8f7c009ed7e86b3e82aee
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-08-02 14:18:33 -0700

    update css and jsx files. and a new image.

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 import './Navber.css'
+import search from '../assets/search.svg'
+import user from '../assets/user.jpg'
 
 const Navber = () => {
   return (
     <header className="header">
-        <nav className="navber">
-            <h1 className="logo">OMWS</h1>
+      <nav className="navber">
+        <h1 className="logo">OMWS</h1>
 
-            <ul className="navber-links">
-                <li>Home</li>
-                <li>Series</li>
-                <li>Tv Shows</li>
-                <li>Movies</li>
-                <li>My Lists</li>
-                <li>Gondras</li>
-            </ul>
+        <ul className="navber-links">
+          <li>Home</li>
+          <li>Series</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>My Lists</li>
+          <li>Gondras</li>
+        </ul>
             
-            <div className="user">
-                <div className="search-box">
-                    
-                </div>
-            </div>
-        </nav>
+        <div className="user">
+          <div className="search-box">
+            <input type="text" placeholder='search-here' /><button className="search-button"><img src={search} className='search-icon' /></button>
+          </div>
+
+          <div className="user">
+            <button className="user-button"><img src={user} className='user-icon' /></button>
+          </div>
+        </div>
+      </nav>
     </header>
   )
 }
