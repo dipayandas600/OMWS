2c9143e78283baf715ce4fa1cf891ad8610e3630
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-08-03 13:24:01 -0700

    update css and jsx files. and add 3 new images.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Slider.css'
+import slider1 from '../assets/slider/slider1.jpg'
+import slider2 from '../assets/slider/slider2.jpg'
+import slider3 from '../assets/slider/slider3.jpg'
+import slider4 from '../assets/slider/slider4.jpg'
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className="slider-container">
+      <div className="sliders">
+        <img src={slider1} className='image-sliders' />
+        <img src={slider2} className='image-sliders' />
+        <img src={slider3} className='image-sliders' />
+        <img src={slider4} className='image-sliders' />
+      </div>
+    </div>
   )
 }
 
